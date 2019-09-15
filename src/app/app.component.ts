4f69f0ded3aa8e74f1788bf3d838363a2c0a6b76
Rajesh P <rajeshp@Rajeshs-MacBook-Pro.local>
2019-09-15 14:41:05 -0500

    Currency, Date and Case Pipe Example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,42 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pipeBasicExamples';
+
+  gymList = [
+    {
+      name: 'Planet Fitness',
+      type: 'Anytime Fitness',
+      location: 'River Ridge',
+      price: '25',
+      currency: 'USD',
+      started: new Date("January 31 1980 ")
+
+    },
+    {
+      name: 'Ochsner Fitness  Centre',
+      type: 'Luxury Fitness',
+      location: 'Elmwood',
+      price: '2100',
+      currency: 'INR',
+      started: new Date(2015, 11, 10)
+    },
+    {
+      name: 'Snap Fitness',
+      type: 'Cross Fit',
+      location: 'Kenner',
+      price: '40',
+      currency: 'CAD',
+      started: new Date(2012, 3, 7)
+    },
+    {
+      name: 'Nola Fitness',
+      type: 'Aerobics/Gymnstics',
+      location: 'Metairie',
+      price: '40',
+      currency: 'GBP',
+      started: new Date(2014, 10, 18)
+
+    }
+  ];
 }
+
